bbb23fab2d9ac6a5444da2f5866c0c08e0a65026
Pedro Morais <88340056+Pedros-Morais@users.noreply.github.com>
2025-06-07 21:35:20 -0300

    finish integration page

--- src/app/dashboard/integracoes/page.tsx
+++ src/app/dashboard/integracoes/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import IntegrationCard from '@/components/integrations/IntegrationCard';
+import IntegrationCard from '../../../components/integrations/IntegrationCard';
 
 export default function IntegracoesPage() {
   const [loading, setLoading] = useState(false);
@@ -13,16 +13,8 @@ export default function IntegracoesPage() {
       name: 'Amazon Web Services',
       description: 'Conecte sua conta AWS para gerenciar recursos e pipelines de infraestrutura.',
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
-          <path d="M11.9991 12.789L7.79908 15.2377V10.3402L11.9991 7.89148L16.1991 10.3402V15.2377L11.9991 12.789Z" />
-          <path d="M11.9991 16.9742L7.79908 19.4229V14.5254L11.9991 12.0767L16.1991 14.5254V19.4229L11.9991 16.9742Z" />
-          <path d="M11.9991 8.61518L7.79908 11.0639V6.16639L11.9991 3.7177L16.1991 6.16639V11.0639L11.9991 8.61518Z" />
-          <path d="M20.3991 10.3402V15.2377L16.1991 12.789V7.89148L20.3991 10.3402Z" />
-          <path d="M20.3991 14.5254V19.4229L16.1991 16.9742V12.0767L20.3991 14.5254Z" />
-          <path d="M20.3991 6.16639V11.0639L16.1991 8.61518V3.7177L20.3991 6.16639Z" />
-          <path d="M3.59908 10.3402L7.79908 7.89148V12.789L3.59908 15.2377V10.3402Z" />
-          <path d="M3.59908 14.5254L7.79908 12.0767V16.9742L3.59908 19.4229V14.5254Z" />
-          <path d="M3.59908 6.16639L7.79908 3.7177V8.61518L3.59908 11.0639V6.16639Z" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor" className="w-8 h-8">
+          <path d="M257.2 162.7c-48.7 1.8-169.5 15.5-169.5 117.5 0 109.5 138.3 114 183.5 43.2 6.5 10.2 35.4 37.5 45.3 46.8l56.8-56S341 288.9 341 261.4V114.3C341 89 316.5 32 228.7 32 140.7 32 94 87 94 136.3l73.5 6.8c16.3-49.5 54.2-49.5 54.2-49.5 40.7-.1 35.5 29.8 35.5 69.1zm0 86.8c0 80-84.2 68-84.2 17.2 0-47.2 50.5-56.7 84.2-57.8v40.6zm136 163.5c-7.7 10-70 67-174.5 67S34.2 408.5 9.7 379c-6.8-7.7 1-11.3 5.5-8.3C88.5 415.2 203 488.5 387.7 401c7.5-3.7 13.3 2 5.5 12zm39.8 2.2c-6.5 15.8-16 26.8-21.2 31-5.5 4.5-9.5 2.7-6.5-3.8s19.3-46.5 12.7-55c-6.5-8.5-37-4.3-48-3.2-10.8 1-13 2-14-.3-2.3-5.7 21.7-15.5 37.5-17.5 15.7-1.8 41-.8 46 5.7 3.7 5.1 0 27.1-6.5 43.1z"/>
         </svg>
       ),
       connected: false,
